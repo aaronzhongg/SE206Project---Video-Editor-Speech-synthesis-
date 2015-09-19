2941f3f26782e8033ce1550f6627e6bab8903cb0
Aaron <azho472@aucklanduni.ac.nz>
2015-09-19 14:02:34 +1200

    changed textbox to JTextArea and added festival

--- src/vidivox/Player.java
+++ src/vidivox/Player.java
@@ -1,5 +1,7 @@
 package vidivox;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -11,6 +13,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
+import javax.swing.SwingWorker;
+
 import java.awt.Font;
 
 import uk.co.caprica.vlcj.binding.LibVlc;
@@ -20,16 +24,24 @@ import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import javax.swing.JTextArea;
+import javax.swing.DropMode;
 
 /*
  * Main menu frame, contains most of the GUI and the media player
  */
 public class Player extends JFrame {
-	
+
 	private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 	private final EmbeddedMediaPlayer video ;
 	volatile private boolean mouseDown = false;
@@ -39,20 +51,20 @@ public class Player extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
+
 		//add vlc search path
 		NativeLibrary.addSearchPath(
-	            RuntimeUtil.getLibVlcLibraryName(), "/Applications/vlc-2.0.0/VLC.app/Contents/MacOS/lib"
-	        );
-	    Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
-	        
+				RuntimeUtil.getLibVlcLibraryName(), "/Applications/vlc-2.0.0/VLC.app/Contents/MacOS/lib"
+				);
+		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Player frame = new Player();
 					frame.setVisible(true);
 					//play big buck bunny, will change later
-			        frame.mediaPlayerComponent.getMediaPlayer().playMedia("sample_video_big_buck_bunny_1_minute.avi");
+					frame.mediaPlayerComponent.getMediaPlayer().playMedia("sample_video_big_buck_bunny_1_minute.avi");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -70,7 +82,7 @@ public class Player extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		//Reverse button
 		JButton btnReverse = new JButton("<<");
 		btnReverse.addMouseListener(new MouseAdapter() {
@@ -78,20 +90,20 @@ public class Player extends JFrame {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				if (e.getButton() == MouseEvent.BUTTON1) {
-			        mouseDown = true;
-			        initVidControlThread("R");	//calls method that make a thread for this to keep GUI responsive
-			    }
+					mouseDown = true;
+					initVidControlThread("R");	//calls method that make a thread for this to keep GUI responsive
+				}
 			}
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				if (e.getButton() == MouseEvent.BUTTON1) {
-			        mouseDown = false;	//the thread will check this to determine when to stop
-			    }
+					mouseDown = false;	//the thread will check this to determine when to stop
+				}
 			}
 		});
 		btnReverse.setBounds(33, 451, 54, 25);
 		contentPane.add(btnReverse);
-		
+
 		//Play and pause button
 		JButton btnPlay = new JButton("Play/Pause");
 		btnPlay.addActionListener(new ActionListener() {
@@ -106,32 +118,41 @@ public class Player extends JFrame {
 		});
 		btnPlay.setBounds(99, 451, 117, 25);
 		contentPane.add(btnPlay);
-		
+
 		//fastforward button
 		JButton btnFastForward = new JButton(">>");
-		
+
 		btnFastForward.addMouseListener(new MouseAdapter() {
-			
+
 			//If held down it will FF, stops FFing when released.
 			@Override
 			public void mousePressed(MouseEvent e) {
 				if (e.getButton() == MouseEvent.BUTTON1) {
-			        mouseDown = true;
-			        initVidControlThread("FF");	//calls method that make a thread for this to keep GUI responsive
-			    }
+					mouseDown = true;
+					initVidControlThread("FF");	//calls method that make a thread for this to keep GUI responsive
+				}
 			}
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				if (e.getButton() == MouseEvent.BUTTON1) {
-			        mouseDown = false;	//the thread will check this to determine when to stop
-			    }
+					mouseDown = false;	//the thread will check this to determine when to stop
+				}
 			}
 		});
-		
+
 
 		btnFastForward.setBounds(228, 451, 70, 25);
 		contentPane.add(btnFastForward);
-		
+
+		final JTextArea txtArea = new JTextArea();
+		txtArea.setWrapStyleWord(true);
+		txtArea.setRows(5);
+		txtArea.setToolTipText("Enter text for test to speech. ");
+		txtArea.setFont(new Font("Dialog", Font.PLAIN, 15));
+		txtArea.setLineWrap(true);
+		txtArea.setBounds(551, 12, 302, 151);
+		contentPane.add(txtArea);
+
 		//Simple mute button
 		JButton btnMute = new JButton("Mute");
 		btnMute.addActionListener(new ActionListener() {
@@ -141,72 +162,109 @@ public class Player extends JFrame {
 		});
 		btnMute.setBounds(316, 451, 70, 25);
 		contentPane.add(btnMute);
-		
-		JButton btnListen = new JButton("Listen");
+
+		final JButton btnListen = new JButton("Listen");
+		btnListen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//BackgroundTask task = new BackgroundTask(txtArea.getText(),btnListen);
+
+				btnListen.setEnabled(false);
+
+				SwingWorker worker = new SwingWorker<Void, Integer>() {
+
+					@Override
+					protected Void doInBackground() throws Exception {
+						ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", "echo " + txtArea.getText() + " | festival --tts");
+						ProcessBuilder waiter = new ProcessBuilder("/bin/bash", "-c", "wait");
+
+
+						try {
+							Process process = builder.start();
+							Process wait = waiter.start();
+
+
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						return null;
+					}
+
+
+					@Override
+					protected void done(){
+						btnListen.setEnabled(true);
+					}
+				};
+
+				
+
+				worker.execute();
+			}
+		});
 		btnListen.setBounds(551, 192, 135, 40);
 		contentPane.add(btnListen);
-		
+
 		JButton btnCreateMp = new JButton("Create mp3");
 		btnCreateMp.setBounds(698, 192, 155, 40);
 		contentPane.add(btnCreateMp);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(556, 12, 297, 144);
-		contentPane.add(textPane);
-		
+
 		JButton btnBrowseMp = new JButton("Browse mp3...");
 		btnBrowseMp.setBounds(551, 267, 155, 40);
 		contentPane.add(btnBrowseMp);
-		
+
 		JLabel lblNewLabel = new JLabel("Insert mp3 path");
 		lblNewLabel.setBounds(556, 313, 297, 15);
 		contentPane.add(lblNewLabel);
-		
+
 		JButton btnNewButton_3 = new JButton("Add Commentary\n");
 		btnNewButton_3.setFont(new Font("Dialog", Font.BOLD, 22));
 		btnNewButton_3.setBounds(551, 365, 302, 111);
 		contentPane.add(btnNewButton_3);
-		
+
 		//panel for video player
 		JPanel playerPanel = new JPanel(new BorderLayout());
 		playerPanel.setBounds(33, 80, 506, 360);
 		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
 		video = mediaPlayerComponent.getMediaPlayer();
+
+
 		playerPanel.add(mediaPlayerComponent, BorderLayout.CENTER);
 		contentPane.add(playerPanel);
-		
+
 		//button for choosing a video to play
 		JButton btnBrowseVideo = new JButton("Browse Video");
 		btnBrowseVideo.setBounds(33, 43, 168, 25);
 		contentPane.add(btnBrowseVideo);
 	}
-	
+
 	/*
 	 * check method to ensure concurrency when multiple events are fired
 	 * This is just in case other events are fired while fastforwarding or reversing (highly unlikely)
 	 */
 	volatile private boolean isRunning = false;
 	private synchronized boolean checkAndMark() {
-	    if (isRunning) return false;
-	    isRunning = true;
-	    return true;
+		if (isRunning) return false;
+		isRunning = true;
+		return true;
 	}
-	
-	private void initVidControlThread(String arg){	                    
+
+	private void initVidControlThread(final String arg){	                    
 		if (checkAndMark()) {	//don't start another thread if this one is still running
-	        new Thread() {
-	            public void run() {
-	                do {
-	                	//Check which button was pressed
-	                	if(arg.equals("FF")){
-	                		video.skip(10);
-	                	}else{
-	                		video.skip(-10);
-	                	}
-	                } while (mouseDown); //do until released
-	                isRunning = false;	//no longer running
-	            }
-	        }.start();	
-	    }
+			new Thread() {
+				public void run() {
+					do {
+						//Check which button was pressed
+						if(arg.equals("FF")){
+							video.skip(10);
+						}else{
+							video.skip(-10);
+						}
+					} while (mouseDown); //do until released
+					isRunning = false;	//no longer running
+				}
+			}.start();	
+		}
 	}
+
+
 }
